8adad83907ef49e3d08f46fcf810a0172570c2b1
Muhammed Besirovic <besirovicmm@outlook.com>
2020-02-10 16:00:02 +0100

    update

--- Digitron/Program.cs
+++ Digitron/Program.cs
@@ -11,23 +11,28 @@ namespace Digitron
         static void Main(string[] args)
         {
 
-            string[] ignorisi = { "-", " ","+","*","/"};
-
-
+            //Unosimo komandu za racunanje
             Console.WriteLine("Unesite dva broja i operaciju. Npr : 1000-200");
             string unos = Console.ReadLine();
 
+            //na kojim mestima da delimo unos
+            string[] ignorisi = { "-", " ", "+", "*", "/"};
 
+            //delimo unos i delove stavljamo u niz "delovi"
             string[] delovi = unos.Split(ignorisi, StringSplitOptions.RemoveEmptyEntries);
 
+            //pretvaramo tekst u brojeve
+            double a = double.Parse(delovi[0]);
+            double b = double.Parse(delovi[1]);
+
+            //Pretrazujemo unos u potrazi za sledecim operacijama
             bool oduzimanje = unos.Contains("-");
             bool sabiranje = unos.Contains("+");
             bool mnozenje = unos.Contains("*");
             bool deljenje = unos.Contains("/");
 
-            double a = double.Parse(delovi[0]);
-            double b = double.Parse(delovi[1]);
-
+            
+            //Obrada i stampanje rezultata
             if (oduzimanje)
             {
                 Console.WriteLine(  );
